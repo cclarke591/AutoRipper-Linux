8b54b68b983f94ff09aef68fb20e479fc667f5c6
Sk8r776 <Steve@Steves-Mac-Pro.local>
2014-11-04 21:25:48 -0500

    DVD Routine added to installer
    Added DVD routine to installer script.

--- installer.sh
+++ installer.sh
@@ -60,8 +60,64 @@ then
 	echo "Updating Aptitude and Installing ABCDE." >> install.log
 	sudo apt-get -q update &>> install.log
     sudo apt-get -y install abcde &>> install.log
-    cp MIMEs/ripcd.desktop /usr/share/applications/ripcd.desktop
-    if [ -f /home/$script_user/.local/share/applications/mimeapps.list]
+    sudo cp MIMEs/ripcd.desktop /usr/share/applications/ripcd.desktop
+	echo "Auto CD Ripping Installed!"
+	echo "Please change the default application for CDs in"
+	echo "System Settings -> Details -> Removable Media"
+	echo "Change CD audio to Rip CD"
+else
+fi
+
+# DVD Ripping install routine
+if [ $installdvdrip -eq "true" ]
+then
+	echo "Updating Aptitude and Installing DVDBackup."
+	echo "Updating Aptitude and Installing DVDBackup." >> install.log
+	sudo add-apt-repository -y ppa:stebbins/handbrake-snapshots &>> install.log
+	sudo apt-get -q update &>> install.log
+   	sudo apt-get -y install dvdbackup git libtool handbrake-cli handbrake-gtk &>> install.log
+   	git clone git://git.videolan.org/libdvdcss
+   	cd libdvdcss
+   	./configure
+   	make && make install
+   	$libdvdcss="installed"
+   	echo '/usr/local/lib' > /etc/ld.so.conf.d/libdvdcss.conf
+   	ldconfig -v
+   	sudo cp Scripts/ripdvd /usr/bin/ripdvd
+   	sudo chmod +x /usr/bin/ripdvd
+   	sudo cp MIMEs/ripdvd.desktop /usr/share/applications/ripdvd.desktop
+	echo "Auto DVD Ripping Installed!"
+	echo "Please change the default application for DVDs in"
+	echo "System Settings -> Details -> Removable Media"
+	echo "Change DVD Video to Rip DVD"
+else
+fi
+
+# BR Ripping install routine
+if [ $installbrrip -eq "true" ]
+then
+#	echo "Updating Aptitude and Installing ABCDE."
+#	echo "Updating Aptitude and Installing ABCDE." >> install.log
+#	sudo apt-get -q update &>> install.log
+#    sudo apt-get -y install abcde &>> install.log
+#    cp MIMEs/ripcd.desktop /usr/share/applications/ripcd.desktop
+#    if [ -f /home/$script_user/.local/share/applications/mimeapps.list]
+#    then
+#    	echo "Existing mimeapps.list found. Moving to .bak file." >> install.log
+#    	mv /home/$script_user/.local/share/applications/mimeapps.list /home/$script_user/.local/share/applications/mimeapps.list.bak
+#		cp MIMEs/mimeapps.list /home/$script_user/.local/share/applications/
+#	else
+#		echo "Copying mimeapps.list." >> install.log
+#		cp MIMEs/mimeapps.list /home/$script_user/.local/share/applications/
+#	fi
+#	echo "Auto CD Ripping Installed!"
+#	echo "Please change the default application for CDs in"
+#	echo "System Settings -> Details -> Removable Media"
+#	echo "Change CD audio to Rip CD"
+else
+fi
+
+if [ -f /home/$script_user/.local/share/applications/mimeapps.list]
     then
     	echo "Existing mimeapps.list found. Moving to .bak file." >> install.log
     	mv /home/$script_user/.local/share/applications/mimeapps.list /home/$script_user/.local/share/applications/mimeapps.list.bak
@@ -70,9 +126,3 @@ then
 		echo "Copying mimeapps.list." >> install.log
 		cp MIMEs/mimeapps.list /home/$script_user/.local/share/applications/
 	fi
-	echo "Auto CD Ripping Installed!"
-	echo "Please change the default application for CDs in"
-	echo "System Settings -> Details -> Removable Media"
-	echo "Change CD audio to Rip CD"
-else
-fi

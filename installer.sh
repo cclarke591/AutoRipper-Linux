45591b4046c32713a1bc6087677aa8556007e86f
Sk8r776 <Steve@Steves-Mac-Pro.local>
2014-11-05 20:43:10 -0500

    Modified and tested installer script
    Testing revealed issues and typos.
    -Fixed variables in IF statements
    -Modified IF statements
    -Modified mimeapps.list location and routine
    -Other misc. fixes

--- installer.sh
+++ installer.sh
@@ -16,21 +16,22 @@ fi
 # we need the ripping user login for the MIME autorun system
 echo "Please enter username: "
 read script_user
-echo "You entered: $script_user is this correct?"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-else
-	exit 1
-fi
+echo "Username to be installed under: $script_user"
+#if [[ $REPLY =~ ^[Yy]$ ]]
+#then
+#	
+#else
+#	exit 1
+#fi
 
 # Ask if we want to install CD Ripping
 read -p "Install auto CD ripping?" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	installcdrip="true"
+	installcdrip="1"
 else
-	installcdrip="false"
+	installcdrip="0"
 fi
 
 # Ask if we want to install DVD Ripping
@@ -38,9 +39,9 @@ read -p "Install auto DVD ripping?" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	installdvdrip="true"
+	installdvdrip="1"
 else
-	installdvdrip="false"
+	installdvdrip="0"
 fi
 
 # Ask if we want to install BR Ripping
@@ -48,16 +49,17 @@ read -p "Install auto Bluray ripping?" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	installbrrip="true"
+	installbrrip="1"
 else
-	installbrrip="false"
+	installbrrip="0"
 fi
 
 # CD Ripping install routine
-if [ $installcdrip -eq "true" ]
+if [ $installcdrip -eq 1 ]
 then
 	echo "Updating Aptitude and Installing ABCDE."
 	echo "Updating Aptitude and Installing ABCDE." >> install.log
+	read -p "Accept any EULAs during this process. Press [Enter] key to continue..."
 	sudo apt-get -q update &>> install.log
     sudo apt-get -y install abcde &>> install.log
     sudo cp MIMEs/ripcd.desktop /usr/share/applications/ripcd.desktop
@@ -66,23 +68,20 @@ then
 	echo "System Settings -> Details -> Removable Media"
 	echo "Change CD audio to Rip CD"
 else
+	echo "CD Ripping not selected for install." >> install.log
 fi
 
 # DVD Ripping install routine
-if [ $installdvdrip -eq "true" ]
+if [ $installdvdrip -eq 1 ]
 then
 	echo "Updating Aptitude and Installing DVDBackup."
 	echo "Updating Aptitude and Installing DVDBackup." >> install.log
+	read -p "Accept any EULAs during this process. Press [Enter] key to continue..."
 	sudo add-apt-repository -y ppa:stebbins/handbrake-snapshots &>> install.log
+	sudo cat "deb http://archive.ubuntu.com/ubuntu precise main universe restricted multiverse" > /etc/apt/sources.list
 	sudo apt-get -q update &>> install.log
-   	sudo apt-get -y install dvdbackup git libtool handbrake-cli handbrake-gtk &>> install.log
-   	git clone git://git.videolan.org/libdvdcss
-   	cd libdvdcss
-   	./configure
-   	make && make install
-   	$libdvdcss="installed"
-   	echo '/usr/local/lib' > /etc/ld.so.conf.d/libdvdcss.conf
-   	ldconfig -v
+   	sudo apt-get -y install dvdbackup git libtool handbrake-cli handbrake-gtk ubuntu-restricted-extras &>> install.log
+   	sudo /usr/share/doc/libdvdread4/install-css.sh
    	sudo cp Scripts/ripdvd /usr/bin/ripdvd
    	sudo chmod +x /usr/bin/ripdvd
    	sudo cp MIMEs/ripdvd.desktop /usr/share/applications/ripdvd.desktop
@@ -91,11 +90,12 @@ then
 	echo "System Settings -> Details -> Removable Media"
 	echo "Change DVD Video to Rip DVD"
 else
+	echo "DVD Ripping not selected for install." >> install.log
 fi
 
 # BR Ripping install routine
-if [ $installbrrip -eq "true" ]
-then
+#if [ $installbrrip -eq "true" ]
+#then
 #	echo "Updating Aptitude and Installing ABCDE."
 #	echo "Updating Aptitude and Installing ABCDE." >> install.log
 #	sudo apt-get -q update &>> install.log
@@ -114,15 +114,12 @@ then
 #	echo "Please change the default application for CDs in"
 #	echo "System Settings -> Details -> Removable Media"
 #	echo "Change CD audio to Rip CD"
-else
-fi
+#else
+#fi
 
-if [ -f /home/$script_user/.local/share/applications/mimeapps.list]
-    then
-    	echo "Existing mimeapps.list found. Moving to .bak file." >> install.log
-    	mv /home/$script_user/.local/share/applications/mimeapps.list /home/$script_user/.local/share/applications/mimeapps.list.bak
-		cp MIMEs/mimeapps.list /home/$script_user/.local/share/applications/
-	else
-		echo "Copying mimeapps.list." >> install.log
-		cp MIMEs/mimeapps.list /home/$script_user/.local/share/applications/
-	fi
+echo "Existing mimeapps.list found. Moving to .bak file." &>> install.log
+echo "Existing mimeapps.list found. Missing file error is OK here!"
+echo "Existing mimeapps.list found. Missing file error is OK here!" &>> install.log
+mv /home/$script_user/.local/share/applications/mimeapps.list /home/$script_user/.local/share/applications/mimeapps.list.bak
+mkdir -p /home/$script_user/.local/share/applications/
+cp MIMEs/mimeapps.list /home/$script_user/.local/share/applications/mimeapps.list

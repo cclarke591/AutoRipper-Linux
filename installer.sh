883d3414a4d3ca63c834a16fc6344b4e5e6bd9da
Sk8r776 <tomaszewski.steve@gmail.com>
2014-11-10 14:15:45 -0500

    Bluray install routine modified
    Tested and fixed bluray install routine
    Added logging for blurry install
    Fixed Ripbluray script

--- installer.sh
+++ installer.sh
@@ -116,46 +116,46 @@ else
 fi
 
 # BR Ripping install routine
-if [ $installbrrip -eq 1]
+if [ $installbrrip -eq 1 ]
 then
 	if [ $installdvdrip -eq 1 ]
 	then
-	echo "Some Pre Reqs have been installed!" &>> install.log
+	echo "Some Pre Reqs have been installed!" &>> BRRIP-install.log
 	else
-	sudo add-apt-repository -y ppa:stebbins/handbrake-snapshots &>> install.log
-	echo 'deb http://download.videolan.org/pub/debian/stable/ /' | sudo sh -c 'cat >> /etc/apt/sources.list' &>> install.log
-	echo 'deb-src http://download.videolan.org/pub/debian/stable/ /' | sudo sh -c 'cat >> /etc/apt/sources.list' &>> install.log
-	wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc|sudo apt-key add - &>> install.log
-	sudo apt-get -q update &>> install.log
-	sudo apt-get -y install git handbrake-cli handbrake-gtk libdvdcss2 &>> install.log
+	sudo add-apt-repository -y ppa:stebbins/handbrake-snapshots &>> BRRIP-install.log
+	echo 'deb http://download.videolan.org/pub/debian/stable/ /' | sudo sh -c 'cat >> /etc/apt/sources.list' &>> BRRIP-install.log
+	echo 'deb-src http://download.videolan.org/pub/debian/stable/ /' | sudo sh -c 'cat >> /etc/apt/sources.list' &>> BRRIP-install.log
+	wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc|sudo apt-key add - &>> BRRIP-install.log
+	sudo apt-get -q update &>> BRRIP-install.log
+	sudo apt-get -y install git handbrake-cli handbrake-gtk libdvdcss2 &>> BRRIP-install.log
 	fi
-	sudo apt-get -y install build-essential libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev libqt4-dev
-	wget http://www.makemkv.com/download/makemkv-bin-1.8.14.tar.gz
-	wget http://www.makemkv.com/download/makemkv-oss-1.8.14.tar.gz
-	tar xvzf makemkv-oss-1.8.14.tar.gz
-	tar xvzf makemkv-bin-1.8.14.tar.gz
-	cd makemkv-oss-1.8.14/
-	./configure
+	sudo apt-get -y install build-essential libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev libqt4-dev &>> BRRIP-install.log
+	wget http://www.makemkv.com/download/makemkv-bin-1.8.14.tar.gz &>> BRRIP-install.log
+	wget http://www.makemkv.com/download/makemkv-oss-1.8.14.tar.gz &>> BRRIP-install.log
+	tar xvzf makemkv-oss-1.8.14.tar.gz &>> BRRIP-install.log
+	tar xvzf makemkv-bin-1.8.14.tar.gz &>> BRRIP-install.log
+	cd makemkv-oss-1.8.14/ &>> BRRIP-install.log
+	./configure &>> BRRIP-install.log
+	make && sudo make install &>> BRRIP-install.log
+	cd .. &>> BRRIP-install.log
+	cd makemkv-bin-1.8.14/ &>> BRRIP-install.log
 	make && sudo make install
-	cd ..
-	cd makemkv-bin-1.8.14/
-	make && sudo make install
-	cd ..
-	rm -R makemkv*
-	sudo cp Scripts/ripbluray /usr/bin/ripbluray
-	sudo chmod +x /usr/bin/ripbluray
-   	sudo cp MIMEs/ripbluray.desktop /usr/share/applications/ripbluray.desktop
+	cd .. &>> BRRIP-install.log
+	rm -R makemkv* &>> BRRIP-install.log
+	sudo cp Scripts/ripbluray /usr/bin/ripbluray &>> BRRIP-install.log
+	sudo chmod +x /usr/bin/ripbluray &>> BRRIP-install.log
+   	sudo cp MIMEs/ripbluray.desktop /usr/share/applications/ripbluray.desktop &>> BRRIP-install.log
    	echo "Auto Bluray Ripping Installed!"
 	echo "Please change the default application for DVDs in"
 	echo "System Settings -> Details -> Removable Media -> Other Media"
 	echo "Change Blu-ray Video Disc to Rip Bluray"
 else
-	echo "Bluray Ripping not selected for install." >> install.log
+	echo "Bluray Ripping not selected for install." &>> BRRIP-install.log
 fi
 
-echo "Existing mimeapps.list found. Moving to .bak file." &>> install.log
+echo "Existing mimeapps.list found. Moving to .bak file." &>> BRRIP-install.log
 echo "Existing mimeapps.list found. Missing file error is OK here!"
-echo "Existing mimeapps.list found. Missing file error is OK here!" &>> install.log
+echo "Existing mimeapps.list found. Missing file error is OK here!" &>> BRRIP-install.log
 mv /home/$script_user/.local/share/applications/mimeapps.list /home/$script_user/.local/share/applications/mimeapps.list.bak
 mkdir -p /home/$script_user/.local/share/applications/
 cp MIMEs/mimeapps.list /home/$script_user/.local/share/applications/mimeapps.list
